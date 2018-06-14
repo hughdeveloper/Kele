37398bc9406b7415dad3ab6e3db5bf8d0dfb8430
hughdeveloper <hughdeveloper@gmail.com>
2018-06-13 17:28:50 -0700

    Initialize

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,19 +5,28 @@ require 'json'
 class Kele
   include HTTParty
   base_uri = 'https://www.bloc.io/api/v1'
-  attr_accessor :url, :authtoken
+  attr_accessor :url, :auth_token
+
+  def new(email, password)
+    initialize(email, password)
+  end
 
   def initialize(email, password)
-    @authtoken = self.class.post(api_url("sessions"), {:query => {email: email, password: password}})
-    raise "Incorrect email or password" if @authtoken == nil
-    @authtoken
+    response = self.class.post(api_url("sessions"), { body: {email: email, password: password}})
+    @auth_token = response["auth_token"]
+    raise "Incorrect Credentials" if @auth_token == nil
+  end
+
+  def get_me
+    response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
+    @user = JSON.parse(response.body)
   end
 
 
 
 
 
-
+  private
   def api_url(endpoint)
     "https://www.bloc.io/api/v1/#{endpoint}"
   end
