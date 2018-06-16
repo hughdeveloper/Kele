7769aa51572489b6c105bac74695e9e27b13c493
hughdeveloper <hughdeveloper@gmail.com>
2018-06-16 14:18:00 -0700

    Mentor Availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -22,8 +22,10 @@ class Kele
     @user = JSON.parse(response.body)
   end
 
-
-
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    @mentor_availability = JSON.parse(response.body)
+  end
 
 
   private
